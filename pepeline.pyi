adaba0cc15a0ef45e6e15e6287f499bc10a819fb
umzi <ab5447612@gmail.com>
2024-03-17 04:37:08 +0300

    add: pyi

---
+++ pepeline.pyi
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import numpy as np
+def read_gray(
+        path:str
+)->np.ndarray: "open gray image"
+def read(
+        path:str
+)->np.ndarray: "open rgb image"
+def screenton(
+        array:np.ndarray,
+        dot_size:int,
+        ly_plus:int|None,
+        lx_plus:int|None,
+)->np.ndarray:"array:np.float32\n dot_size:uint,ly and lx_plus: uint, if None == dot_size//2 "
+def fast_color_level(
+        array: np.ndarray,
+        in_low: int,
+        in_high: int,
+        out_low: int,
+        out_high: int,
+        gamma: float,
+)->np.ndarray: " array:np.float32 \n in_low...out_high:uint8 \n gamma:float32"
+def normolize(
+        array: np.ndarray
+)->np.ndarray:"array:np.float32"
+def save(
+        array: np.ndarray,
+        path:str
+)->np.ndarray:"array:np.uint8"
